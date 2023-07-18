d525402f625d8bc86fac8932a8ea16bb6f9e19a3
Crowdbotics <engineering@crowdbotics.com>
2023-07-18 14:03:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "matchmade-55-dev-102615",
+  url: "https://matchmade-55-dev-102615.botics.co",
+  api: "https://matchmade-55-dev-102615.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
